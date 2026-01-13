bcdc0c7f072ca023f4cab8caba3fb7c278e353b1
thiagocamargodev <tigrejobs22@gmail.com>
2026-01-12 22:46:12 -0300

    image path correction 2

--- data.js
+++ data.js
@@ -92,7 +92,7 @@ window.balasData = [
     {
         "id": 11,
         "nome": "Coco Beijinho Gourmet",
-        "imagens": ["images/balabeijinho.jpg"],
+        "imagens": ["images/balaBeijinho.jpg"],
         "descricao": "Bala de coco com recheio cremoso de beijinho, unindo a suavidade do coco a uma doçura clássica e aconchegante. A textura macia e o sabor delicado criam um doce simples, tradicional e irresistível.",
         "ingredientes": "Leite de Coco, Açúcar, recheio de brigadeiro de beijinho.",
         "sabor": "Doce suave com coco cremoso.",
